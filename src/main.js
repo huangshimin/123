85b30512ddbb39b91dcf09fd52cefc76832fba75
autumnfish <517729329@qq.com>
2019-05-06 10:26:45 +0800

    项目准备工作

--- src/main.js
+++ src/main.js
@@ -1,8 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+// 饿了吗ui
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+Vue.use(ElementUI)
+
+// 导入路由
+import router from './router'
+
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
+  // 挂在到vue实例上
+  router
 }).$mount('#app')
---
+++ src/router.js
@@ -0,0 +1,19 @@
+// 导入vue
+import Vue from 'vue'
+// 导入VueRouter
+import VueRouter from 'vue-router'
+// use一下
+Vue.use(VueRouter)
+
+// 导入组件
+
+// 路由规则
+const routes = []
+
+// 实例化路由对象
+const router = new VueRouter({
+  routes
+})
+
+// 暴露出去
+export default router
