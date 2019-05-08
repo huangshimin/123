9003e1199ffdd6ce2f84c38251a6d107c538b6e1
autumnfish <517729329@qq.com>
2019-05-08 11:23:49 +0800

    登录页-表单验证

--- src/components/login.vue
+++ src/components/login.vue
@@ -2,15 +2,22 @@
   <div class="login">
     <div class="center-box">
       <h2 class="my-h3">用户登录</h2>
-      <el-form label-position="top" label-width="80px">
-        <el-form-item label="用户名">
-          <el-input></el-input>
+      <el-form
+        :model="loginForm"
+        :rules="loginRules"
+        ref="loginForm"
+        label-position="top"
+        label-width="80px"
+      >
+        <el-form-item label="用户名" prop="username">
+          <el-input v-model="loginForm.username"></el-input>
         </el-form-item>
-        <el-form-item label="密码">
-          <el-input></el-input>
+        <el-form-item label="密码" prop="password">
+          <el-input v-model="loginForm.password" type="password"></el-input>
         </el-form-item>
         <el-form-item>
-          <el-button class="my-btn" type="primary">登录</el-button>
+          <el-button type="primary" @click="submitForm('loginForm')">登录</el-button>
+          <el-button type="danger" @click="resetForm('loginForm')">重置</el-button>
         </el-form-item>
       </el-form>
     </div>
@@ -20,10 +27,59 @@
 <script>
 export default {
   name: "login",
+  data() {
+    return {
+      // 表单数据
+      loginForm: {
+        username: "",
+        password: ""
+      },
+      // 表单验证规则
+      loginRules: {
+        username: [
+          // 必须            提示内容                    触发方式
+          { required: true, message: "请输入用户名", trigger: "blur" },
+          // 最短    最长     提示信息                         触发方式
+          { min: 5, max: 8, message: "长度在 5 到 8 个字符", trigger: "change" }
+        ],
+        password: [
+          // 必须            提示内容                    触发方式
+          { required: true, message: "请输入密码", trigger: "blur" },
+          // 最短    最长     提示信息                         触发方式
+          {
+            min: 6,
+            max: 12,
+            message: "长度在 6 到 12 个字符",
+            trigger: "change"
+          }
+        ]
+      }
+    };
+  },
+  // 方法
+  methods: {
+    // 提交表单
+    submitForm(formName) {
+      // 通过ref属性 获取表单 并且调用验证的方法
+      this.$refs[formName].validate(valid => {
+        if (valid) {
+          // 数据格式没问题
+        } else {
+          // 数据有问题
+          this.$message.error("哥们，数据格式不对哦，你是机器人吗？");
+          return false;
+        }
+      });
+    },
+    // 重置表单
+    resetForm(formName) {
+      this.$refs[formName].resetFields();
+    }
+  },
   // 生命周期 钩子
   created() {
-    this.$request.sayHi()
-  },
+    this.$request.sayHi();
+  }
 };
 </script>
 
